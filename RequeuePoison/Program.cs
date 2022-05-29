1740107197c953f9715f12f0f536edf442595cb6
Neil Boyd <NeilBoyd@gmail.com>
2022-05-29 21:43:50 +0200

    convert to .net 6

--- RequeuePoison/Program.cs
+++ RequeuePoison/Program.cs
@@ -1,107 +1,91 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 
-namespace RequeuePoison
+// read the configuration
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true)
+    .AddJsonFile($"appsettings.{environmentName}.json", true)
+    .AddUserSecrets<Program>()
+    .AddCommandLine(args);
+var configuration = builder.Build();
+
+// get queue name
+var queueName = configuration["Queue"] ?? throw new ArgumentException("Specify queue name");
+var poisonQueueName = $"{queueName}-poison";
+
+// get storage references
+var connectionString = configuration["ConnectionString"] ?? throw new ArgumentException("Specify connection string");
+var storageAccount = CloudStorageAccount.Parse(connectionString);
+var queueClient = storageAccount.CreateCloudQueueClient();
+var queue = queueClient.GetQueueReference(queueName);
+var poisonQueue = queueClient.GetQueueReference(poisonQueueName);
+
+// get max property
+var maxString = configuration["Max"];
+if (!int.TryParse(maxString, out var max))
 {
-    /// <summary>
-    /// Requeue messages from poison queue back to original queue.
-    /// Requeue messages from the last 24 hours, delete older messages.
-    /// </summary>
-    public class Program
+    max = int.MaxValue;
+}
+
+// get max age property
+var maxAgeString = configuration["MaxAge"];
+if (!int.TryParse(maxAgeString, out var maxAge))
+{
+    maxAge = 1;
+}
+
+// get interval property
+var intervalString = configuration["Interval"];
+if (!int.TryParse(intervalString, out var intervalInt))
+{
+    intervalInt = 0;
+}
+var interval = TimeSpan.FromMilliseconds(intervalInt);
+
+var requeued = 0;
+var deleted = 0;
+var ageLimit = DateTime.UtcNow.AddDays(-maxAge);
+var delay = TimeSpan.Zero;
+while (true)
+{
+    var message = await poisonQueue.GetMessageAsync();
+    if (message == null)
     {
-        public static async Task Main(string[] args)
-        {
-            // read the configuration
-            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true)
-                .AddJsonFile($"appsettings.{environmentName}.json", true)
-                .AddUserSecrets<Program>()
-                .AddCommandLine(args);
-            var configuration = builder.Build();
-
-            // get queue name
-            var queueName = configuration["Queue"] ?? throw new ArgumentException("Specify queue name");
-            var poisonQueueName = $"{queueName}-poison";
-
-            // get storage references
-            var connectionString = configuration["ConnectionString"] ?? throw new ArgumentException("Specify connection string");
-            var storageAccount = CloudStorageAccount.Parse(connectionString);
-            var queueClient = storageAccount.CreateCloudQueueClient();
-            var queue = queueClient.GetQueueReference(queueName);
-            var poisonQueue = queueClient.GetQueueReference(poisonQueueName);
-
-            // get max property
-            var maxString = configuration["Max"];
-            if (!int.TryParse(maxString, out var max))
-            {
-                max = int.MaxValue;
-            }
-
-            // get max age property
-            var maxAgeString = configuration["MaxAge"];
-            if (!int.TryParse(maxAgeString, out var maxAge))
-            {
-                maxAge = 1;
-            }
-
-            // get interval property
-            var intervalString = configuration["Interval"];
-            if (!int.TryParse(intervalString, out var intervalInt))
-            {
-                intervalInt = 0;
-            }
-            var interval = TimeSpan.FromMilliseconds(intervalInt);
-
-            var requeued = 0;
-            var deleted = 0;
-            var ageLimit = DateTime.UtcNow.AddDays(-maxAge);
-            var delay = TimeSpan.Zero;
-            while (true)
-            {
-                var message = await poisonQueue.GetMessageAsync();
-                if (message == null)
-                {
-                    break;
-                }
-                var isOld = message.InsertionTime < ageLimit;
-                if (isOld)
-                {
-                    ++deleted;
-                }
-                else
-                {
-                    await queue.AddMessageAsync(new CloudQueueMessage(message.AsString), null, delay, null, null);
-                    ++requeued;
-                    delay += interval;
-                }
-                await poisonQueue.DeleteMessageAsync(message);
-                if (requeued == max)
-                {
-                    break;
-                }
-            }
-
-            Console.WriteLine($"Requeued {requeued} messages to {queueName}, deleted {deleted}");
-            if(delay < TimeSpan.FromMinutes(3))
-            {
-                Console.WriteLine($"Total delay: {delay.TotalSeconds} seconds");
-            }
-            else if(delay < TimeSpan.FromHours(3))
-            {
-                Console.WriteLine($"Total delay: {(delay.TotalSeconds / 60):F1} minutes");
-            }
-            else
-            {
-                Console.WriteLine($"Total delay: {(delay.TotalSeconds / 3600):F1} hours");
-            }
-            Console.WriteLine("Enter ENTER to continue");
-            Console.ReadLine();
-        }
+        break;
+    }
+    var isOld = message.InsertionTime < ageLimit;
+    if (isOld)
+    {
+        ++deleted;
+    }
+    else
+    {
+        await queue.AddMessageAsync(new CloudQueueMessage(message.AsString), null, delay, null, null);
+        ++requeued;
+        delay += interval;
+    }
+    await poisonQueue.DeleteMessageAsync(message);
+    if (requeued == max)
+    {
+        break;
     }
 }
+
+Console.WriteLine($"Requeued {requeued} messages to {queueName}, deleted {deleted}");
+if(delay < TimeSpan.FromMinutes(3))
+{
+    Console.WriteLine($"Total delay: {delay.TotalSeconds} seconds");
+}
+else if(delay < TimeSpan.FromHours(3))
+{
+    Console.WriteLine($"Total delay: {(delay.TotalSeconds / 60):F1} minutes");
+}
+else
+{
+    Console.WriteLine($"Total delay: {(delay.TotalSeconds / 3600):F1} hours");
+}
+Console.WriteLine("Enter ENTER to continue");
+Console.ReadLine();
